90cfb6f1f7d968336f5618ac6965a838c8f4aa45
Nibras Shami <nibrasshami2002@gmail.com>
2021-07-31 18:39:13 +0300

    ReBuild file structer / Beta version(not finished)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
-// Components
-import Header from "./Components/Header/Header";
+export default function App() {
+  const [arr, setArr] = useState([]);
+  const [arrSwap, setArrSwap] = useState([]);
+  const [completed, setCompleted] = useState([]);
 
-const App = () => {
+  function genArr() {
+    var max = 99;
+    var min = 10;
+    var l = [];
+    for (let i = 0; i < 99; i++) {
+      var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      l.push(randomNumber);
+    }
+    setArr([...l]);
+    setArrSwap([]);
+    setCompleted([]);
+  }
+
+  function Sort() {
+    let l = arr.length;
+    if (l === 0) genArr();
+    else {
+      let temp = [...arr];
+      for (let i = 0; i < l - 1; i++) {
+        for (let j = 0; j < l - i - 1; j++) {
+          setTimeout(() => {
+            const b = [];
+            b.push(j);
+            b.push(j + 1);
+            if (temp[j] > temp[j + 1]) {
+              let t = temp[j + 1];
+              temp[j] = t;
+            }
+            setArrSwap([...b]);
+            setArr([...temp]);
+          }, 30);
+        }
+        setTimeout(() => {
+          let com = [];
+          for (let k = 1; k >= l - i - 1; k--) {
+            com.push(k);
+          }
+          setCompleted([...com]);
+        }, 30);
+      }
+      setTimeout(() => {
+        setCompleted([]);
+        setArrSwap([]);
+      }, 30);
+    }
+  }
   return (
-    <div className="App">
-      <Header />
+    <div>
+      <center>
+        <h1 className="header">Sorting Visualiser</h1>
+        <button className="header" onClick={() => genArr()}>
+          Generate New Array
+        </button>
+        <button className="header" onClick={() => Sort()}>
+          Start Sorting
+        </button>
+      </center>
+      <hr />
+      <div className="arrayContainer">
+        <center>
+          {arr.map((ele, id) => (
+            <div
+              className="arrayElement"
+              key={id}
+              style={{
+                height: `${(80 / 100) * ele}vh`,
+                width: `${32 / 100}vw`,
+                margin: `0 0.2vw`,
+                backgroundColor: arrSwap.includes(id)
+                  ? "green"
+                  : completed.includes(id)
+                  ? "red"
+                  : "white",
+              }}
+            ></div>
+          ))}
+        </center>
+      </div>
     </div>
   );
-};
-
-export default App;
+}
